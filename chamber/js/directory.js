7c1ebb6f448ecdd28e4104e5e611ec3b0e6f09b4
Julieth-H <martirjulieth@byui.edu>
2023-06-28 13:20:37 -0500

    change data

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,62 +1,65 @@
 
-const url = 'https://julieth-h.github.io/wdd230/chamber/json/data.json';
 
-async function getData() {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    displaydata(data.jason);
-  } catch (error) {
+  const urlData = "https://julieth-h.github.io/wdd230/chamber/json/data.json";
+  async function getdata() {
+    try{
+    const respnse = await fetch(url);
+    const data = await respnse.json();
+    displeycards(data.json);
+    }  catch (error) {
     console.error('Error:', error);
   }
-  }
- /*getdivcards();*/
-  function displayBusinessInfo(entries) {
-   /* const = document.querySelector('business-container');*/
+}
 
+  const displayentries = (entries) => {
+    const grid = document.querySelector('div.grid');
 
-    }
-    //add cards to div
-    const displayBusiness = (business) => {
-        const cards = document.querySelector("div.cards");
-        business.forEach((item) => {
-            let card = document.createElement("section");
-            let image = document.createElement("img");
-            let name = document.createElement("h3");
-            let address1 = document.createElement("p");
-            let phone = document.createElement("p");
-            let category = document.createElement("P");
-            let web = document.createElement("a");
-    
-            image.setAttribute("src", item.image);
-            image.setAttribute("alt", `imagre of ${item.name}`);
-            image.setAttribute("loading", "lazy");        
-            
-            name.textContent = `${item.name}`;
-            address1.textContent = `${item.address[0]}`;
-            phone.textContent = `${item.phone}`;
-            web.innerHTML = `${item.web}`;            
-            web.setAttribute("href", `${item.web}`);
-            card.appendChild(name);
-            card.appendChild(image);
-            card.appendChild(address1);
-            card.appendChild(phone);
-            card.appendChild(category);
-            card.appendChild(web);
-            
-            cards.appendChild(card);
-        });
-    }
-    //buttons to change views
-    const gridbutton = document.querySelector("#grid");
-    const listbutton = document.querySelector("#list");
-    const display = document.querySelector("article");
-    //add class 'list' to article
-    listbutton.addEventListener("click", () => {
-        display.classList.add("list");
+    entries.forEach((company) => {
+      let card = document.createElement('section');
+      let logo = document.createElement('img');
+      let name = document.createElement('h2');
+      let address = document.createElement('p');
+      let phone = document.createElement('p');
+      let category = document.createElement('p');
+      let website = document.createElement('a');
+
+      name.innerHTML = `${company.name}`;
+      address.innerHTML = `${company.address}`;
+      phone.innerHTML = `${company.phone}`;
+      website.innerHTML = `${company.website}`;
+      website.href = `${company.website}`;
+
+      logo.setAttribute('src', company.imageurl);
+      logo.setAttribute('alt', `Logo of ${company.name}`);
+      logo.setAttribute('loading', 'lazy');
+
+      card.appendChild(logo);
+      card.appendChild(name);
+      card.appendChild(address);
+      card.appendChild(phone);
+      card.appendChild(category);
+      card.appendChild(website);
+
+      grid.appendChild(card);
     });
-    //remove class 'list' from article
-    gridbutton.addEventListener("click", () => {
-        display.classList.remove("list");
-    })
-     
+  }
+
+  getCompanyData(urlData)
+    .then((companies) => {
+      displayCompanies(companies);
+    });
+
+  const gridbutton = document.querySelector("#grid");
+  const listbutton = document.querySelector("#list");
+  const display = document.querySelector(".grid");
+
+  gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+  });
+
+  listbutton.addEventListener("click", () => {
+    display.classList.remove("grid");
+    display.classList.add("list");
+  });
+
