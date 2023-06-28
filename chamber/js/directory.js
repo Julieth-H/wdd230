837acb5f3c3f0b364743b420f3c9a317b4dfc160
Julieth-H <martirjulieth@byui.edu>
2023-06-27 20:00:13 -0500

    datajson

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,19 +1,48 @@
-const requestURL = 'json/data.json';
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('json/data.json')
+      .then(response => response.json())
+      .then(data => displayBusinessInfo(data.entries))
+      .catch(error => console.error('Error fetching JSON data:', error));
+  });
 
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+  function displayBusinessInfo(entries) {
+    const businessContainer = document.getElementById('business-container');
 
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
+    entries.forEach(entry => {
+      const entryContainer = document.createElement('div');
+      entryContainer.classList.add('entry');
 
-listbutton.addEventListener("click", showList); // example using defined function
+      const image = document.createElement('img');
+      image.src = entry.image;
+      image.alt = entry.name;
+      image.classList.add('entry-image');
 
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}
+      const name = document.createElement('h2');
+      name.textContent = entry.name;
+
+      const address = document.createElement('p');
+      address.textContent = 'Address: ' + entry.address;
+
+      const phone = document.createElement('p');
+      phone.textContent = 'Phone: ' + entry.phone;
+
+      const website = document.createElement('p');
+      const websiteLink = document.createElement('a');
+      websiteLink.href = entry.website;
+      websiteLink.textContent = 'Website';
+      website.appendChild(websiteLink);
+
+      const category = document.createElement('p');
+      category.textContent = 'Category: ' + entry.category;
+
+      entryContainer.appendChild(image);
+      entryContainer.appendChild(name);
+      entryContainer.appendChild(address);
+      entryContainer.appendChild(phone);
+      entryContainer.appendChild(website);
+      entryContainer.appendChild(category);
+
+      businessContainer.appendChild(entryContainer);
+    });
+  }
+
