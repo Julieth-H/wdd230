271ba05480035227c7c39d6200361e2b3534bfcb
Julieth-H <martirjulieth@byui.edu>
2023-06-30 00:17:21 -0500

    Changes

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -2,62 +2,64 @@
 document.addEventListener('DOMContentLoaded', () => {
   const urldata = "https://julieth-h.github.io/wdd230/chamber/json/data.json";
 
-  async function getbusinessdata(urldata) {
-    const response = await fetch(urldata);
+  async function getBusinessData(url) {
+    const response = await fetch(url);
     const data = await response.json();
     return data.business;
-
- 
   }
 
-  const displaybusiness = (business) => {
-    const grid = document.querySelector('div.grid');
+  const displayBusiness = (businesses) => {
+    const grid = document.querySelector('.cards');
 
-    business.forEach((business) => {
+    businesses.forEach((business) => {
       let card = document.createElement('section');
-      let image = document.createElement('image');
+      let logo = document.createElement('img');
       let name = document.createElement('h2');
       let address = document.createElement('p');
       let phone = document.createElement('p');
-      let category = document.createElement('p');
       let website = document.createElement('a');
 
-      name.innerHTML = `${business.name}`;
-      image.innerHTML =$`{business.image}`;
-      address.innerHTML = `${business.address}`;
-      phone.innerHTML = `${business.phone}`;
-      website.innerHTML = `${business.website}`;
-      website.href = `${business.website}`;
+      name.innerHTML = business.name;
+      logo.setAttribute('src', business.logourl);
+      address.innerHTML = business.address;
+      phone.innerHTML = business.phone;
+      website.innerHTML = business.website;
+      website.href = business.website;
 
-      image.setAttribute('src', business.imageurl);
-      image.setAttribute('alt', `logo of ${business.name}`);
-      image.setAttribute('loading', 'lazy');
+      logo.setAttribute('alt', `logo of ${business.name}`);
+      logo.setAttribute('loading', 'lazy');
+      logo.setAttribute('width', '140');
+      logo.setAttribute('height', '150');
+      logo.src = 'path/to/default/image.jpg'; // Replace with the URL of a default image
 
-      card.appendChild(image);
+      card.appendChild(logo);
       card.appendChild(name);
       card.appendChild(address);
       card.appendChild(phone);
       card.appendChild(website);
-      card.appendChild(category);
+
       grid.appendChild(card);
     });
   }
 
-  getbusinessdata(urldata)
-    .then((business) => {
-      displaybusiness(business);
+  getBusinessData(urldata)
+    .then((businesses) => {
+      displayBusiness(businesses);
+    })
+    .catch((error) => {
+      console.log('Error:', error);
     });
 
-  const gridbutton = document.querySelector("#grid");
-  const listbutton = document.querySelector("#list");
-  const display = document.querySelector(".grid");
+  const gridButton = document.querySelector("#grid");
+  const listButton = document.querySelector("#list");
+  const display = document.querySelector(".cards");
 
-  gridbutton.addEventListener("click", () => {
+  gridButton.addEventListener("click", () => {
     display.classList.add("grid");
     display.classList.remove("list");
   });
 
-  listbutton.addEventListener("click", () => {
+  listButton.addEventListener("click", () => {
     display.classList.remove("grid");
     display.classList.add("list");
   });
